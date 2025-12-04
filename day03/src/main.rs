5a37c72d05106947fa17de7168e179ed1cca6967
Andreas Henriksson <andreas@fatal.se>
2025-12-04 10:18:02 +0100

    day03: part2 - WRONG ANSWER!

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -25,13 +25,16 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let file = File::open("input.txt")?;
     let reader = BufReader::new(file);
     let mut bignum_sum: i32 = 0;
+    let mut second_sum: i64 = 0;
 
     for linei in reader.lines() {
         let line = linei?;
 
         println!("Line: {}", line);
 
+        // ========== part 1 ==========
 
+        /*
         let (first_num, first_offset) = find_biggest(&line[..line.len()-1]);
 
         println!("First biggest num: {}, offset: {}", first_num, first_offset);
@@ -45,9 +48,54 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         println!("Biggest number: {}", bignum);
 
         bignum_sum += bignum;
+        */
+
+        // ========== part 2 ==========
+        let mut skipped = 0;
+        let mut next_start = 0;
+        let mut result: i64  = 0;
+        let skip_limit = line.len() - 12;
+
+        while skipped < skip_limit {
+            let last_search: bool = result >= 10_000_000_000;
+
+            // sliding window of skip_limit, unless last - all the way to the end.
+            let end_offset = if last_search { line.len()-1 } else if skipped == 0 { next_start+skip_limit-1 } else { next_start+skip_limit-skipped };
+
+            println!("Debug: searching {}..={}", next_start, end_offset);
+            let search_part = &line[next_start..=end_offset];
+
+            let (num, offset) = find_biggest(search_part);
+            println!("Biggest num (in search part {}): {}, offset: {}", search_part, num, offset);
+
+            skipped += offset;
+            next_start += offset + 1;
+
+            result = result * 10 + num as i64;
+
+            println!("Debug: skipped:{}, result:{}", skipped, result);
+
+            if last_search {
+                break;
+            }
+        }
+
+        let mut remainder = "";
+        if skipped == skip_limit {
+            remainder = &line[next_start..line.len()-skip_limit+skipped];
+        }
+
+        let res: i64 = (result.to_string() + remainder).parse().unwrap();
+        println!("Result: {}", res);
+        second_sum += res;
+        println!("DebugResult: {}#{}", result, remainder);
     }
 
+    // ========== part 1 ==========
     println!("Sum of biggest numbers: {}", bignum_sum);
+
+    // ========== part 2 ==========
+    println!("Second sum: {}", second_sum);
     
     Ok(())
 }
